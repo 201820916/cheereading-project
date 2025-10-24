e3d037948ce96df99d8468ed713d98b38edea3ec
201820916 <kkupsh2@gmail.com>
2025-10-24 14:05:34 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
-pip install --upgrade pip
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
 pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+python manage.py migrate
